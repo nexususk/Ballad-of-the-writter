5ab9a65f693a8d0448802ee0363e33d831893a6f
Simón García <simongarciab@gmail.com>
2026-03-04 16:05:45 -0500

    Palabras al azar en script

---
+++ Assets/Scripts/Diccionario.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Diccionario : MonoBehaviour
+{
+    public TextAsset dic;
+    public string[] arregloDic;
+    public string palabraActual;
+    public string letra;
+
+    void Start()
+    {
+        arregloDic = dic.text.Split(new char[] { ' ' });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ObtenerLetraPresionada() != null){
+            letra = ObtenerLetraPresionada();
+            if(letra.Equals(palabraActual.Substring(0, 1).ToUpper()))
+            {
+                palabraActual = palabraActual.Substring(1);
+            }
+            else
+            {
+                print("ERROR");
+            }
+                
+        }
+    }
+    public void AsignarPalabra(int cual)
+    {
+        palabraActual = arregloDic[cual];
+    }
+
+    [ContextMenu("Generar palabra al azar")]
+    public void ElegirPalabraAzar()
+    {
+        AsignarPalabra(Random.Range(0,arregloDic.Length));
+    }
+    public string ObtenerLetraPresionada()
+    {
+        if (Input.inputString.Length > 0)
+        {
+            char c = Input.inputString[0];
+
+            // Verifica si es letra (incluye ñ automáticamente)
+            if (char.IsLetter(c))
+            {
+                return char.ToUpper(c).ToString();
+            }
+        }
+
+        return null;
+    }
+}
