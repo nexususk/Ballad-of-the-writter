33362aba3099af2318d4e617bd15429304a08d08
Simon <simongarciab@gmail.com>
2026-04-28 11:56:34 -0500

    Inicio del menu co boton de juego util

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+
+
+    public void Juego()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
